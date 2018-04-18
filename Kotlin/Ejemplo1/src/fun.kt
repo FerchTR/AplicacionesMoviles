ad79b0e1778fe4ab01959b84f562efa7f92ac87f
FerchTR <eddyferch@hotmail.es>
2018-04-18 09:53:20 -0500

    primeras lineas

---
+++ Kotlin/Ejemplo1/src/fun.kt
@@ -0,0 +1,13 @@
+fun sumarDosNumeros(numeroUno:Int, numeroDos:Int):Int{
+
+    return numeroUno + numeroDos
+
+}
+
+fun main(args:Array<String>){
+
+    println("Hola mundo")
+    sumarDosNumeros(numeroUno = 1, numeroDos = 2)
+    println(sumarDosNumeros(numeroUno = 1, numeroDos = 2))
+
+}
